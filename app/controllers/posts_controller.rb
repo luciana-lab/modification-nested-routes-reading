006494ddd645882870934949233c5b3e4b6b5bf8
Luciana Sugeta <lulusugeta@gmail.com>
2021-07-16 11:58:21 -0400

    commit tree b5366f9b64afd7603c2c946fcaf59b4c5a0a97e5

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,8 +16,18 @@ class PostsController < ApplicationController
     end
   end
 
+
   def new
-    @post = Post.new
+    # ensure that we're creating a new post for a valid author
+    if params[:author_id] && !Author.exists?(params[:author_id])
+      redirect_to authors_path, alert: "Author not found."
+      # /authors
+    else
+    # We want to make sure that, if we capture an author_id through a nested route, 
+    # we keep track of it and assign the post to that author.
+    # /authors/:author_id/posts/new
+    @post = Post.new(author_id: params[:author_id])
+    end
   end
 
   def create
@@ -27,18 +37,33 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
+    @post = Post.find_by_id(params[:id])
     @post.update(post_params)
     redirect_to post_path(@post)
   end
 
+  # make sure that: 
+  # 1) the author_id is valid
+  # 2) the post matches the author.
   def edit
-    @post = Post.find(params[:id])
+    if params[:author_id]
+      author = Author.find_by_id(params[:author_id])
+      if author.nil?
+        redirect_to authors_path, alert: "Author not found."
+        # /authors
+      else
+        @post = author.posts.find_by_id(params[:id])
+        redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+        # /authors/:author_id/posts
+      end
+    else
+      @post = Post.find_by_id(params[:id])
+    end
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
